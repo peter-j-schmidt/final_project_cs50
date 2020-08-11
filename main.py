bd7907bac99de823c80d37440b34038a6f20c18e
Peter Schmidt <mlbp912@gmail.com>
2020-08-11 16:08:43 -0400

    Added code to login to service.

--- main.py
+++ main.py
@@ -1,18 +1,59 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, jsonify, redirect, session
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPExceptions
-from werkzeug.security import check_password
+from werkzeug.exceptions import default_exceptions, HTTPExceptions, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
 
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 app = Flask(__name__)
 
+#Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+
+
 @app.route("/")
-def hello_world():
+@login_required
+def index():
     return render_template("layout.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log in user"""
+
+    #forget any user_id
+    sesson.clear()
+
+    # User reached route via POST
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        #Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
